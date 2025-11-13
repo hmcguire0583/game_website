daa7e96a91d1b7fd73d3702daf08557f3b0fe9da
hmcguire0583 <harryjunior2026@gmail.com>
2025-11-13 00:28:27 -0500

    fixed configs for vercel deployment

---
+++ math-survival-game/next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Add your config options here
+  reactStrictMode: true,
+  swcMinify: true,
+  // etc.
+};
+
+module.exports = nextConfig;
