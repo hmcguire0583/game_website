266e2d29e342c1728138591946119c1364f42733
João Ziedins <Joao>
2025-11-20 15:38:44 -0500

    timerrr

--- game/scenes/timer.gd
+++ game/scenes/timer.gd
@@ -3,8 +3,6 @@ extends Node2D
 @export var time_limit := 60  # seconds
 @onready var level_timer := $LevelTimer
 @onready var timer_label := $TimerLabel
-@onready var question_label := $QuestionLabel
-@onready var answer_input := $AnswerInput
 @onready var objective_label := $CanvasLayer2/ObjectiveLabel
 
 var current_enemy = null
@@ -16,7 +14,7 @@ func _ready():
 		level_timer.one_shot = true
 		level_timer.start()
 		level_timer.timeout.connect(_on_time_out)
-		objective_label.text = "Objective: Defeat all enemies"
+		objective_label.text = "Objective: Solve Math Questions to Defeat All Enemies"
 	else:
 		push_error("LevelTimer node not found!")
 
@@ -35,24 +33,3 @@ func _on_time_out():
 		timer_label.text = "GAME OVER"
 	# Change scene to Main Menu
 	get_tree().change_scene_to_file("res://main_menu.tscn")
-	
-func ask_question(enemy):
-	current_enemy = enemy
-	var a = randi() % 10 + 1
-	var b = randi() % 10 + 1
-	correct_answer = a + b
-	question_label.text = "Solve: %d + %d =" % [a, b]
-	answer_input.text = ""
-	answer_input.editable = true
-	answer_input.grab_focus()
-
-func _on_AnswerInput_text_submitted(new_text):
-	if int(new_text) == correct_answer and current_enemy:
-		print("Correct! Enemy defeated.")
-		current_enemy.queue_free()
-		current_enemy.check_all_enemies_defeated()
-	else:
-		print("Wrong answer!")
-	answer_input.editable = false
-	question_label.text = ""
-	current_enemy = null
