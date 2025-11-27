d0df161cc290861dbcb9f6031062792f0ff3e010
hmcguire0583 <harryjunior2026@gmail.com>
2025-11-26 23:27:28 -0500

    added main_menu music and day/night cycle

---
+++ game/scripts/DayNight.gd
@@ -0,0 +1,72 @@
+extends Node2D
+class_name DayNight
+
+signal time_changed(current_hour: float, time_string: String)
+
+@onready var canvas_modulate: CanvasModulate = $CanvasModulate
+
+@export_group("Time Settings")
+@export var day_duration: float = 24.0
+@export var starting_hour: float = 8.0
+@export_range(0,23) var sunrise_hour: int = 6
+@export_range(0,23) var sunset_hour: int = 19
+
+@export_group("Color Settings")
+@export var day_color: Color = Color(1.0, 1.0, 1.0, 1.0)
+@export var night_color: Color = Color(0.02, 0.02, 0.05, 0.85)
+@export var sunset_color: Color = Color(0.84, 0.58, 0.28, 1.0)
+@export var sunrise_color: Color = Color(0.86, 0.70, 0.70, 1.0)
+
+var elapsed_time: float = 0.0
+var current_hour: float = 0.0
+
+func _ready() -> void:
+	current_hour = starting_hour
+	_update_world_color()
+	time_changed.emit(current_hour, get_time_string())
+
+func _process(delta: float) -> void:
+	elapsed_time += delta
+	current_hour = fmod(starting_hour + (elapsed_time * 24.0 / (day_duration * 60.0)), 24.0)
+	_update_world_color()
+	time_changed.emit(current_hour, get_time_string())
+
+func _update_world_color() -> void:
+	var target_color: Color
+	
+	if current_hour >= sunrise_hour and current_hour < sunrise_hour + 1:
+		var t = (current_hour - sunrise_hour) / 1.0
+		target_color = night_color.lerp(day_color, t)
+	elif current_hour >= sunrise_hour + 1 and current_hour < sunset_hour - 2:
+		target_color = day_color
+	elif current_hour >= sunset_hour - 2 and current_hour < sunset_hour:
+		var t = (current_hour - (sunset_hour - 2)) / 2.0
+		target_color = day_color.lerp(sunset_color, t)
+	elif current_hour >= sunset_hour and current_hour < sunset_hour + 2:
+		var t = (current_hour - sunset_hour) / 2.0
+		target_color = sunset_color.lerp(night_color, t)
+	else:
+		target_color = night_color
+		
+	canvas_modulate.color = target_color
+
+func get_time_string() -> String:
+	var hour := floori(current_hour)
+	var minute := floori((current_hour - hour) * 60)
+	var ampm := "AM" if hour < 12 else "PM"
+	if hour == 0:
+		hour = 12
+	elif hour > 12:
+		hour -= 12
+	return "%d:%02d %s" % [hour, minute, ampm]
+
+func pause_cycle() -> void:
+	set_process(false)
+
+func resume_cycle() -> void:
+	set_process(true)
+
+func set_time(hour: float) -> void:
+	current_hour = hour
+	_update_world_color()
+	time_changed.emit(current_hour, get_time_string())
--- game/scripts/world_2.gd
+++ game/scripts/world_2.gd
@@ -1,14 +1,22 @@
 extends Node2D
 
-@export var time_limit := 30  # seconds
+@export var time_limit := 2400  # seconds
 @onready var level_timer := $LevelTimer
 @onready var timer_label := $TimerLabel
 @onready var objective_label := $CanvasLayer2/ObjectiveLabel
+@onready var day_night : DayNight = $DayNight
+@onready var time_label: Label = $TimeLabel
+@export var enable_day_night_cycle: bool = true
 
 var current_enemy = null
 var correct_answer = 0
 
 func _ready():
+	if enable_day_night_cycle:
+		_setup_day_night_cycle()
+	else:
+		day_night.visible = false
+		day_night.process_mode = Node.PROCESS_MODE_DISABLED
 	GameManager.enemies_defeated = 0
 	if level_timer:
 		level_timer.wait_time = time_limit
@@ -22,7 +30,13 @@ func _ready():
 	# Initialize the label
 	if timer_label:
 		timer_label.text = str(time_limit) + "s"
+func _setup_day_night_cycle() -> void:
+	day_night.time_changed.connect(_on_time_changed)
 
+func _on_time_changed(_hour: float, time_string: String) -> void:
+	if time_label:
+		time_label.text = time_string
+		
 func _process(delta):
 	if level_timer:
 		timer_label.text = str(ceil(level_timer.time_left)) + "s"
